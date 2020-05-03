961d515ae4fbf3c778cbabc835a1a97f3533b12c
Válter <47797461+wurzy@users.noreply.github.com>
2020-05-03 16:43:38 +0100

    fixed useless methods in catmull

--- 3/Engine/catmull-rom.h
+++ 3/Engine/catmull-rom.h
@@ -25,8 +25,6 @@ class CatmullRom {
 
 	float pos[3];
 	float deriv[3];
-	float z[3];
-	float up[3] = { 0,1,0 };
 
 	int segments;
 	float* rgb;
@@ -34,55 +32,13 @@ class CatmullRom {
 	GLuint* buffer;
 
 private:
-	void buildRotMatrix(float* x, float* y, float* z, float* m) {
-
-		m[0] = x[0]; m[1] = x[1]; m[2] = x[2]; m[3] = 0;
-		m[4] = y[0]; m[5] = y[1]; m[6] = y[2]; m[7] = 0;
-		m[8] = z[0]; m[9] = z[1]; m[10] = z[2]; m[11] = 0;
-		m[12] = 0; m[13] = 0; m[14] = 0; m[15] = 1;
-	}
-
-	void transpose(float* a, float* b) { // apenas para 4x4
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				b[4 * i + j] = a[4 * j + i];
-			}
-		}
-	}
-
-	void cross(float* a, float* b, float* res) {
-
-		res[0] = a[1] * b[2] - a[2] * b[1];
-		res[1] = a[2] * b[0] - a[0] * b[2];
-		res[2] = a[0] * b[1] - a[1] * b[0];
-	}
-
-
-	void normalize(float* a) {
-
-		float l = sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
-		a[0] = a[0] / l;
-		a[1] = a[1] / l;
-		a[2] = a[2] / l;
-	}
-
-
-	float length(float* v) {
-
-		float res = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-		return res;
-
-	}
-
 	void multMatrixVector(float* m, float* v, float* res) {
-
 		for (int j = 0; j < 4; ++j) {
 			res[j] = 0;
 			for (int k = 0; k < 4; ++k) {
 				res[j] += v[k] * m[j * 4 + k];
 			}
 		}
-
 	}
 
 	void getCatmullRomPoint(float t, Point p0, Point p1, Point p2, Point p3, float* pos, float* deriv) {
@@ -159,21 +115,12 @@ public:
 	}
 
 	void animatedTranslate() {
-		float m[4][4], m_transpose[4][4];
 		this->gt = glutGet(GLUT_ELAPSED_TIME) / this->animationTime; // elapsed time
 		this->gt -= floor(gt); // has to be between 0 and 1
 
 		getGlobalCatmullRomPoint(gt, pos, deriv);
 
-		//normalize(deriv);
-		//cross(deriv, up, z);
-		//normalize(z);
-		//cross(z, deriv, up);
-		//normalize(up);
 		glTranslatef(pos[0], pos[1], pos[2]); // put the model in place
-		//buildRotMatrix(deriv, up, z, *m); // build the rotation matrix
-		//transpose(*m, *m_transpose); // transpose the resulting rotation matrix
-		//glMultMatrixf(*m_transpose); // final transformations
 	}
 	
 	bool isValid() {
